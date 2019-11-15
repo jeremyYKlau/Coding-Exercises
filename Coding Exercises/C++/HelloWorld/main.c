885b8e2bccd2dc8457f3fd725afe81f987c9c28e
unknown <jeremyyklau@gmail.com>
2019-11-15 10:18:52 -0700

    Just somewhere to store coding exercies

---
+++ Coding Exercises/C++/HelloWorld/main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+//compile c code
+//gcc -o name.exe main.c 
+main(){
+	printf("Hello world");
+}
---
+++ Coding Exercises/C++/HelloWorld/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+//C++ compile line, name is name of executable and main is the main file that has to run
+//g++ -o name.exe main.cpp
+main(){
+	std::cout << "Hello world" << std::endl;
+}
